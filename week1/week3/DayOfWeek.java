87c56a34906b45e9cf9411f6cf1e639e08e02b48
vtu29375-collab <vtu29375@veltech.edu.in>
2026-08-25 10:20:53 +0530

    Add files via upload

---
+++ week1/week3/DayOfWeek.java
@@ -0,0 +1,21 @@
+package week3;
+
+import java.time.LocalDate;
+import java.util.*;
+
+public class DayOfWeek {
+    public static String dayOfTheWeek(int day, int month, int year) {
+        LocalDate date = LocalDate.of(year, month, day);
+        return date.getDayOfWeek().toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int day = sc.nextInt();
+        int month = sc.nextInt();
+        int year = sc.nextInt();
+        System.out.println(dayOfTheWeek(day, month, year));
+        sc.close();
+    }
+}
+
